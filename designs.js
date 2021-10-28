c3aa0c99375b76993a1a5aab4a4fa4b8d9265c6c
Kairi Sülla <kairi.sulla@gmail.com>
2021-10-28 15:23:43 +0300

    Event listener for submit and makeGrid function done

---
+++ designs.js
@@ -0,0 +1,33 @@
+// Select color input
+let gridColor = document.getElementById("colorPicker");
+
+// Select size input
+let gridHeight = document.getElementById("inputHeight");
+let gridWidth = document.getElementById("inputWidth");
+
+// Table
+let grid = document.getElementById("pixelCanvas");
+
+// Form
+let form = document.getElementById("sizePicker");
+
+// When size is submitted by the user, call makeGrid()
+form.addEventListener("submit", function (event){
+  event.preventDefault();
+  makeGrid(gridHeight, gridWidth);
+
+});
+
+// Function makeGrid()
+function makeGrid(height, width){
+  // Table style
+  grid.style.width = "100%";
+  grid.style.border = "1px solid black";
+
+  for (var i=0; i<height.value; i++){
+    let row = grid.insertRow(i);
+    for (let x=0; x<width.value; x++){
+      let cell = row.insertCell(x);
+    };
+  };
+};
