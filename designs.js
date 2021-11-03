7182572dd1798585c295594557dc441230381eb7
Kairi Sülla <kairi.sulla@gmail.com>
2021-11-03 22:56:39 +0200

    Meaningful comments added

--- designs.js
+++ designs.js
@@ -11,19 +11,22 @@ let grid = document.getElementById("pixelCanvas");
 // Form
 let form = document.getElementById("sizePicker");
 
-let cell = '';
 // When size is submitted by the user, call makeGrid()
 form.addEventListener("submit", function (event){
+  // Prevents the default event from happening
   event.preventDefault();
+  // Calls the makeGrid function
   makeGrid(gridHeight, gridWidth);
 
 });
 
 // Function makeGrid()
 function makeGrid(height, width){
+  // deletes the previous table
   grid.innerHTML='';
+  // Defines the style of the table
   grid.style.border = "1px solid black";
-
+  // Runs the for loop for making the grid with height and width values from form
   for (var i=0; i<height.value; i++){
     let row = grid.insertRow(i);
     for (let x=0; x<width.value; x++){
@@ -32,6 +35,8 @@ function makeGrid(height, width){
   };
 };
 
+// Adds an eventlistener for a click in the grid
 grid.addEventListener("click", function (click){
+  // targets the cell that was clicked and colors it the value chosen in the colorPicker
   click.target.style.backgroundColor = gridColor.value;
 });
